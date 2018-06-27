e60bbc2b369493e506972bc68681a29018b7d616
Pujan Maharjan <puzan2072@gmail.com>
2018-06-27 14:41:37 +0545

    add generic views for poll app

--- polls/views.py
+++ polls/views.py
@@ -3,36 +3,29 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 # from django.template import loader
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
 
-    context = {
-        'latest_question_list': latest_question_list
-    }
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    template = 'polls/index.html'
+    def get_queryset(self):
+        """Return the last five published questions."""
 
-    return render(request, template, context)
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    try:
-
-        question = Question.objects.get(pk = question_id) #question = get_object_or_404(Question, pk=question_id)
-        choices = question.choice_set.all()
-    except Question.DoesNotExist:
-        raise Http404("Question Does Not Exists")
-
-    return render(request,'polls/detail.html', {'question':question, 'choices':choices})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/results.html', { 'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name  = 'polls/results.html'
 
 
 
