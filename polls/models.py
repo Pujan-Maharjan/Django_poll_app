2cec9a30b1fc8d370d088ef9e49994791b3dfc68
Pujan Maharjan <puzan2072@gmail.com>
2018-07-05 14:23:06 +0545

    add attributes to was_published_recently method to customize in admin site

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
         return now-datetime.timedelta(days=1)<= self.pub_date<=now
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field= 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "published recently?"
+
 
     def __str__(self):
         return self.question_text
